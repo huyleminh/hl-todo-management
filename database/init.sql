a9639f59e2acecf849e7f100ed236c791b813b8b
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2022-03-12 23:29:22 +0700

    refactor db

--- database/init.sql
+++ database/init.sql
@@ -15,9 +15,8 @@ default collate = utf8mb4_bin;
 
 create table if not exists user (
 	user_id int unsigned auto_increment,
-    username varchar(50) not null unique,
-    password varchar(255) not null,
     email varchar(50) not null unique,
+    password varchar(255) not null,
     user_type tinyint unsigned default 0, -- 0: normal, 1: google, 2: facebook
     date_of_birth date,
     first_name varchar(50) not null,
@@ -30,4 +29,23 @@ create table if not exists user (
 )
 engine = InnoDB
 default character set = utf8mb4
-default collate = utf8mb4_bin
+default collate = utf8mb4_bin;
+
+create table if not exists token (
+	user_id int unsigned,
+	token_uuid varchar(36),
+    refresh_token text not null,
+    created_date timestamp default current_timestamp,
+    last_updated_date timestamp default current_timestamp on update current_timestamp,
+    expires_date timestamp,
+    
+    constraint PF_TOKEN primary key(user_id, token_uuid)
+)
+engine = InnoDB
+default character set = utf8mb4
+default collate = utf8mb4_bin;
+
+alter table token
+add constraint FK_USER_TOKEN
+foreign key (user_id)
+references user(user_id);
